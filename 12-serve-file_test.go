ebd7087e003dcd0ddf892f4d19725d65b64c91ae
rasyidev <habib.rasyid11@gmail.com>
2022-09-18 20:00:39 +0700

    modified:   12-serve-file_test.go

--- 12-serve-file_test.go
+++ 12-serve-file_test.go
@@ -1,6 +1,8 @@
 package learngoweb
 
 import (
+	_ "embed"
+	"fmt"
 	"net/http"
 	"testing"
 )
@@ -27,3 +29,27 @@ Coba:
 localhost:9090
 localhost:9090?name=RasyidevPro
 */
+
+//go:embed resources/noname.html
+var resourceNoName string
+
+//go:embed resources/ok.html
+var resourceOk string
+
+func EmbedServeFile(writer http.ResponseWriter, request *http.Request) {
+	if request.URL.Query().Get("name") == "" {
+		fmt.Fprint(writer, resourceNoName)
+	} else {
+		fmt.Fprint(writer, resourceOk)
+	}
+	// embed
+}
+
+func TestEmbedServeFile(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:9090",
+		Handler: http.HandlerFunc(EmbedServeFile),
+	}
+
+	server.ListenAndServe()
+}
